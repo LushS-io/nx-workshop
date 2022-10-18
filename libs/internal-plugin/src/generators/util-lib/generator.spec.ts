64a62baef2c450b64dd3978f74cb8c40a1677817
Troy Kirinhakone <troy@lushs.io>
2022-10-18 15:00:27 -0700

    migrate to end of lab 18

--- libs/internal-plugin/src/generators/util-lib/generator.spec.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.spec.ts
@@ -1,20 +1,21 @@
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
 import generator from './generator';
 import { UtilLibGeneratorSchema } from './schema';
 
 describe('util-lib generator', () => {
   let appTree: Tree;
-  const options: UtilLibGeneratorSchema = { name: 'test', directory: 'store' };
+  const options: UtilLibGeneratorSchema = { name: 'foo', directory: 'store' };
 
   beforeEach(() => {
     appTree = createTreeWithEmptyWorkspace();
   });
 
-  it('should run successfully', async () => {
+  it('should add util to the name and add appropriate tags', async () => {
     await generator(appTree, options);
-    const config = readProjectConfiguration(appTree, 'store-util-test');
-    expect(config.tags).toEqual(['type:util', 'scope:store']);
+    const config = readProjectConfiguration(appTree, 'store-util-foo');
     expect(config).toBeDefined();
+    expect(config.tags).toEqual(['type:util', 'scope:store']);
   });
 });
